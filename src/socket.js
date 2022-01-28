8d17d01db22c02871e81f025c9dad4be619e2c71
Utkarsh Sharma <utkarshrishu1@gmail.com>
2022-01-28 19:00:18 +0530

    new change

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
-const CONNECTION_PORT='wss://chat-app-serverr.herokuapp.com/ws';
+const CONNECTION_PORT='wss://chat-app-serverr.herokuapp.com';
 const socket=io.connect(CONNECTION_PORT);
 export default socket;
